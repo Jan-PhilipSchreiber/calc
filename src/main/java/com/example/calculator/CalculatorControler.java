8c5f18da4738faaf22ed5a279d9eff35122d1b9f
JanSchreiber <wi21b118@technikum-wien.at>
2022-05-19 15:56:09 +0200

    p2

---
+++ src/main/java/com/example/calculator/CalculatorControler.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorControler {
+
+    @RequestMapping("/sum")
+    public int sum(
+          @RequestParam int a,
+          @RequestParam int b){
+        return a+b;
+    }
+}
