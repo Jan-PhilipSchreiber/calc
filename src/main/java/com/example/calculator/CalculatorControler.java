f4af80ae7273beb63bf161e01695a3df2633c3ca
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-21 16:37:04 +0200

    added div 0 and test

--- src/main/java/com/example/calculator/CalculatorControler.java
+++ src/main/java/com/example/calculator/CalculatorControler.java
@@ -1,11 +1,11 @@
 package com.example.calculator;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class CalculatorControler {
 
     Calculator calculator = new Calculator();
